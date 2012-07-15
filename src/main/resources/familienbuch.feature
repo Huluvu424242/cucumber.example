f068516f233e7dfb5f3f114495ed586b72a23b6c
Thomas Schubert <funthomas424242@googlemail.com>
2012-07-15 21:09:51 +0200

    annotations changed for german language

--- src/main/resources/familienbuch.feature
+++ src/main/resources/familienbuch.feature
@@ -6,6 +6,6 @@ Feature: Erstellen eines Familienbuches
 
 		@german @deutsch
         Scenario: Ahnen erfassen
-                Gegeben ist die Datei family.gedcoml
+                Gegebensei die Datei family.gedcoml
                 Wenn ein Abschnitt beginnt mit Person { und endet mit }
                 Dann soll eine Person ins Familienbuch aufgenommen werden.
--- src/test/java/com/github/funthomas424242/gedcoml/test/FamilienbuchFeature.java
+++ src/test/java/com/github/funthomas424242/gedcoml/test/FamilienbuchFeature.java
@@ -1,29 +1,30 @@
 package com.github.funthomas424242.gedcoml.test;
 
-import cuke4duke.annotation.Pending;
-
-import cuke4duke.annotation.I18n.EN.Given;
-import cuke4duke.annotation.I18n.EN.Then;
-import cuke4duke.annotation.I18n.EN.When;
+import cuke4duke.annotation.I18n.DE.Gegebensei;
+import cuke4duke.annotation.I18n.DE.Dann;
+import cuke4duke.annotation.I18n.DE.Wenn;
+import cuke4duke.annotation.I18n.DE.Und;
 import static junit.framework.Assert.assertEquals;
 
-@SuppressWarnings({"UnusedDeclaration"})
+@SuppressWarnings({ "UnusedDeclaration" })
 public class FamilienbuchFeature {
-	
-    private String fileName;
-    private String name;
-    
-    @Given("^ist die Datei ([A-z]*)$")
-    public void theActionIs(String fileName) {
-        this.fileName = fileName;
-    }
-    
-    @When("^Wenn ein Abschnitt beginnt mit Person \\{ ([A-z]*)$ und endet mit \\}")
-    public void theSubjectIs(String name) {
-        this.name = name;
-    }
-    @Then("^soll eine Person ([^\\.]*).$ ins Familienbuch aufgenommen werden. ")
-    public void theGreetingIs(String name) {
-        assertEquals(String.format("%s, %s", fileName, name), name);
-    }
+
+	private String fileName;
+	private String name;
+
+	@Gegebensei("^die Datei ([A-z]*)$")
+	public void theActionIs(String fileName) {
+		this.fileName = fileName;
+	}
+
+	@Wenn("^ein Abschnitt beginnt mit Person \\{ ([A-z]*)$")
+	@Und("endet mit \\}")
+	public void theSubjectIs(String name) {
+		this.name = name;
+	}
+
+	@Dann("^soll eine Person ([^\\.]*).$ ins Familienbuch aufgenommen werden. ")
+	public void theGreetingIs(String name) {
+		assertEquals(String.format("%s, %s", fileName, this.name), name);
+	}
 }
