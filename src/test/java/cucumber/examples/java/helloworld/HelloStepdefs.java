d6a381a8b22f5743a71b9dd0003850e28b78551a
Thomas Schubert <funthomas424242@googlemail.com>
2012-07-15 19:45:49 +0200

    initial check in

---
+++ src/test/java/cucumber/examples/java/helloworld/HelloStepdefs.java
@@ -0,0 +1,29 @@
+package cucumber.examples.java.helloworld;
+
+import cuke4duke.annotation.Pending;
+
+import cuke4duke.annotation.I18n.EN.Given;
+import cuke4duke.annotation.I18n.EN.Then;
+import cuke4duke.annotation.I18n.EN.When;
+import static junit.framework.Assert.assertEquals;
+
+@SuppressWarnings({"UnusedDeclaration"})
+public class HelloStepdefs {
+	
+    private String action;
+    private String subject;
+    
+    @Given("^The Action is ([A-z]*)$")
+    public void theActionIs(String action) {
+        this.action = action;
+    }
+    
+    @When("^The Subject is ([A-z]*)$")
+    public void theSubjectIs(String subject) {
+        this.subject = subject;
+    }
+    @Then("^The Greeting is ([^\\.]*).$")
+    public void theGreetingIs(String greeting) {
+        assertEquals(String.format("%s, %s", action, subject), greeting);
+    }
+}
